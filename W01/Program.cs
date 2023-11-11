da8db06f0f2abe1ec9c9c6e631df1528fb877266
canberk ustaoglu <76068350+canberkustaoglu@users.noreply.github.com>
2023-11-11 13:47:31 +0300

    Proje dosyası ekle.

---
+++ W01/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace W01 //pcakage, library, coverage ode and object location
+{
+
+    class Program
+    {
+        // starting or entry point method/function
+
+        int a = 1;
+        static void Main(string[] args)
+        {
+            int b = 1 + 3;
+            Console.WriteLine("Hello World!!!");//printf in C
+        }
+
+    }
+
+
+}
+
+// Tokens:
+// 01- keywords
+// 02- identifiers, must be obey the rules of variable definitions
+// 03- symbols
+// 04- operators
+// 05- Literals
+//      numerical
+//      string
